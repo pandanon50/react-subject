82085d1f2fd237ea1bd4f147be76b255f16df2e7
pandanon50 <panda_non@donga.ac.kr>
2021-07-18 14:17:01 +0900

    feat : add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./Routes";
+
 import { Provider } from "react-redux";
-import rootReducer from "./Modules";
-import { rootSaga } from "./Modules";
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import rootReducer, { rootSaga } from "./Modules";
+import Routes from "./Routes";
+
 import "antd/dist/antd.css";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
-  // ,
-  // (window as any).__REDUX_DEVTOOLS_EXTENSION__
-  //   ? composeWithDevTools()
-  //   : (f) => f
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
